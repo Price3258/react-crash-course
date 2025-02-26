6a88e8d2a46d94d94df6cd34863109c416236622
김원태 <rladnjsxo123@naver.com>
2025-02-26 18:53:57 +0900

    23. css 모듈로 css 스타일 적용하기

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,12 @@
+import classes from "./Post.module.css";
+
 function Post(props) {
   const { author, body } = props;
+
   return (
-    <div>
-      <p>{author}</p>
-      <p>{body}</p>
+    <div className={classes.post}>
+      <p className={classes.author}>{author}</p>
+      <p className={classes.text}>{body}</p>
     </div>
   );
 }
