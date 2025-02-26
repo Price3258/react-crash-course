77e5257f890270082f73dd4eb8cc81c05862b345
김원태 <rladnjsxo123@naver.com>
2025-02-26 17:49:16 +0900

    Update Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,10 @@
+const names = ["Maximilian", "Manuel"];
+
 function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
   return (
     <div>
-      <p>price</p>
+      <p>{chosenName}</p>
       <p>react.js is awesome!</p>
     </div>
   );
