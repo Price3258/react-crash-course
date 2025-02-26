01e20ef0d9f019e4259937972fc3720c1f8836fe
김원태 <rladnjsxo123@naver.com>
2025-02-26 21:32:25 +0900

    45 loader() 로 데이터 가져오기

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,38 +1,27 @@
+import { useLoaderData } from "react-router-dom";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
 
 const PostsList = () => {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
+  const posts = useLoaderData();
 
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      const response = await fetch("http://localhost:8080/posts");
-      const resData = await response.json();
-      setPosts(resData.posts);
-      setIsFetching(false);
-    }
-    fetchPosts();
-  }, []);
-
-  function addPostHandler(postData) {
-    try {
-      fetch("http://localhost:8080/posts", {
-        method: "POST",
-        body: JSON.stringify(postData),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      setPosts((prevPosts) => {
-        return [postData, ...prevPosts];
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // function addPostHandler(postData) {
+  //   try {
+  //     fetch("http://localhost:8080/posts", {
+  //       method: "POST",
+  //       body: JSON.stringify(postData),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     setPosts((prevPosts) => {
+  //       return [postData, ...prevPosts];
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
   const renderPosts = posts.map((post) => {
     return <Post key={post.body} author={post.author} body={post.body} />;
@@ -40,17 +29,12 @@ const PostsList = () => {
 
   return (
     <>
-      {!isFetching && posts.length > 0 && (
-        <ul className={classes.posts}>{renderPosts}</ul>
-      )}
-      {!isFetching && posts.length === 0 && (
+      {posts.length > 0 && <ul className={classes.posts}>{renderPosts}</ul>}
+      {posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
         </div>
-      )}
-      {isFetching && (
-        <p style={{ textAlign: "center", color: "white" }}>Loading posts...</p>
       )}
     </>
   );
