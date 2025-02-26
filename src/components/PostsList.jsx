b50070f1be35bc4a68b51690ca34a6a2e4cede4f
김원태 <rladnjsxo123@naver.com>
2025-02-26 19:02:52 +0900

    25. 상태이용 준비하기

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,18 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 const PostsList = () => {
   return (
-    <ul className={classes.posts}>
-      <Post author="Max" body="Reat.js is awesome" />
-      <Post author="Test" body="Test is good" />
-      <Post author="Manuel" body="good" />
-      <Post author="sample" body="sample body" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Max" body="Reat.js is awesome" />
+        <Post author="Test" body="Test is good" />
+        <Post author="Manuel" body="good" />
+        <Post author="sample" body="sample body" />
+      </ul>
+    </>
   );
 };
 
