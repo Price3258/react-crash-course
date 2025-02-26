1e7db97d2ffee8207232c9b36efd398dcc9ea947
김원태 <rladnjsxo123@naver.com>
2025-02-26 19:48:06 +0900

    30. 조건부 렌더링

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import classes from "./PostsList.module.css";
 import Modal from "./Modal";
 
 const PostsList = () => {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function changeBodyHandler(event) {
     setEnteredBody(event.target.value);
@@ -18,12 +23,14 @@ const PostsList = () => {
 
   return (
     <>
-      <Modal>
-        <NewPost
-          onBodyChange={changeBodyHandler}
-          onAuthorChange={onAuthorChange}
-        />
-      </Modal>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={changeBodyHandler}
+            onAuthorChange={onAuthorChange}
+          />
+        </Modal>
+      )}
 
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
