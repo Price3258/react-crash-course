bbbefee1d3c4d156603c3cd468b3fb6f1412725e
김원태 <rladnjsxo123@naver.com>
2025-02-26 21:17:26 +0900

    44 경로 연결 및 경로 탐색

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import classes from "./Modal.module.css";
 
-function Modal({ children, onClose }) {
+function Modal({ children }) {
+  const navigate = useNavigate();
+
+  function closeHanlder() {
+    navigate("..");
+  }
+
   return (
     <>
-      <div className={classes.backdrop} onClick={onClose} />
+      <div className={classes.backdrop} onClick={closeHanlder} />
       <dialog open className={classes.modal}>
         {children}
       </dialog>
